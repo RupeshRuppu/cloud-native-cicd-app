e33122b1757c2df197d3340a379949ea1e83e72c
Rupesh Lakshman <codewithruppu@gmail.com>
2026-05-12 21:36:09 +0530

    health/

--- server/base/urls.py
+++ server/base/urls.py
@@ -1,6 +1,8 @@
+from django.http import HttpResponse
 from django.urls import path, include
 
 urlpatterns = [
+    path("health", lambda request: HttpResponse("OK"), name="health"),
     path("", include("apis.urls")),
     path("auth/", include("auth.urls")),
 ]
